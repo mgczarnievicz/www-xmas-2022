ebf5e5ace1d50ebd37566a4bf53c9c8720f384a9
mgczanievicz <gimena@spiced-academy.com>
2022-12-14 11:12:34 +0100

    coaches description

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,12 +18,13 @@ const coachArray = [
         name: "Gimena",
         img: "/img/coaches/gimena.jpg",
         description:
-            "Is a FS Coach from Spiced Berlin.  Originally for Uruguay and been here in Berlin for 1.5 year. Not only has she done here the web developer bootcamp but also study electronic engineer in her country. ",
+            "is a Web Development Coach from Spiced Berlin.  Originally from Uruguay she moved to Berlin 1.5 years ago. She is a graduate from the web developer bootcamp at Spiced and also studied electronic engineering in her home country. ",
     },
     {
         name: "Jan",
         img: "/img/coaches/jan.png",
-        description: "FS Coach from Spiced Berlin",
+        description:
+            "is a Web Development Coach from Spiced Berlin. He worked as a Software Engineer for 10 years after playing Poker professionally. He is german and loves dogs and coding.",
     },
 ];
 
